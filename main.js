50b49d2a7483529f82766466046505ee00349586
sina-byn <sina.sina.bayan@protonmail.com>
2024-11-14 15:42:13 +0330

    chore: tokenColors added based on esprima tokens

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import * as esprima from 'esprima';
 
 import './style.css';
+
+const tokenColors = {
+  Boolean: '#5653ff',
+  Identifier: '#FD4C87',
+  Keyword: '#F78C6C',
+  Null: '#F78C6C',
+  Numeric: '#F78C6C',
+  Punctuator: '#89DDFF',
+  String: '#C3E88D',
+  RegularExpression: '#89DDFF',
+  Template: '#C3E88D',
+};
